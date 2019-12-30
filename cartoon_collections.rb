45ee74c1e2573b2d5f9ec2dec9e2b85dfe3947da
Todd Carlson <todd.carlson3030@me.com>
2019-12-30 20:10:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map { |planeteer| planeteer.capitalize + "!" } 
   end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
 end
 
 def find_the_cheese# code an argument here
