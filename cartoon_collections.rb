46f68c1e6629d5ca278252df7cffa842cccb33d0
Todd Carlson <todd.carlson3030@me.com>
2019-12-30 21:23:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,5 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  if array.include(cheese_types)
+  if array.map.include(cheese_types) { |cheese| cheese } 
 end
