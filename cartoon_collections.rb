632e56baf85484701a0486da8e802927de6ea378
Todd Carlson <todd.carlson3030@me.com>
2019-12-30 20:45:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,6 @@ def summon_captain_planet(planeteer_calls)
 
 def long_planeteer_calls(planeteer_calls)
  if planeteer_calls.select { |item| item.length > 4 }
-   return true 
  elsif 
   planeteer_calls.select { |item| item.length <= 4 }
   return false 
