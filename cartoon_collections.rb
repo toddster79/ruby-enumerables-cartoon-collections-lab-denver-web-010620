60704754b344d9a00bdfd20a675e8877f635c60d
Todd Carlson <todd.carlson3030@me.com>
2019-12-30 19:38:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+  array.each_with_index { |dwarf| 
+    puts dwarf }
 end
 
 def summon_captain_planet# code an argument here
