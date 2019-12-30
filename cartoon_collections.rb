0d60974dcd5010026df8881b8887dfbe6d0a8f86
Todd Carlson <todd.carlson3030@me.com>
2019-12-30 21:01:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,6 @@ def long_planeteer_calls(planeteer_calls)
   end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
