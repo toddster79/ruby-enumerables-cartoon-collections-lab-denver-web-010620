6c9005235d4beace2baf85ddf7f7020b1b1872f3
Todd Carlson <todd.carlson3030@me.com>
2020-01-14 16:57:25 -0700

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,33 @@
-def roll_call_dwarves(array) 
-  array.each_with_index { |dwarf, index| 
-    puts "#{index + 1}. #{dwarf}\n" } 
-end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" } 
+
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index| 
+    puts "#{index + 1} #{dwarf}"
   end
-
-def long_planeteer_calls(planeteer_calls)
- calls = planeteer_calls.select { |item| item.length > 4 }
-  
-   if calls.reduce {|item1, item2| item1 || item2}
-    return true
-  else
-    return false
-  end 
 end
 
-def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  array.find { |a| cheese_types.include?(a) }
+def summon_captain_planet(dwarves) 
+  dwarves.map do |dwarf| 
+    "#{dwarf.capitalize}!"
+  end
+end 
+
+def long_planeteer_calls(calls) 
+  calls.select do |call| 
+     if call.length > 4 
+      return true
+     end
+  end
+  false
+end 
+
+def find_the_cheese(snacks) 
+  if snacks.include?("cheddar")
+    return "cheddar"
+  elsif snacks.include?("gouda")
+    return "gouda"
+  elsif snacks.include?("camembert")
+    return "camembert"
+  end
+  nil
 end
